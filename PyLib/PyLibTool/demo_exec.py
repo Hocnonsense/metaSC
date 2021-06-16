dcf51195f30a5571045b7d15f148bddd1eaa902d
Hwrn <1322053653@qq.com>
2021-06-16 17:07:14 +0800

    demo of cmd execable files

---
+++ PyLib/PyLibTool/demo_exec.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+ * @Date: 2021-06-06 18:01:40
+ * @LastEditors: Hwrn
+ * @LastEditTime: 2021-06-07 20:56:33
+ * @FilePath: /metaSC/PyLib/PyLibTool/demo_exec.py
+ * @Description:
+"""
+
+from datetime import datetime
+import logging
+import argparse
+from typing import Tuple
+
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    return 0
+
+
+def get_args() -> Tuple:
+    parser = argparse.ArgumentParser(description=__doc__)
+    set_args(parser)
+    args = parser.parse_args()
+    logging.basicConfig(level=args.loglevel.upper())  # info
+
+    print(parser.print_help())
+
+    return
+
+
+def set_args(parser: argparse.ArgumentParser):
+    parser.add_argument('--loglevel', default='INFO', type=str,
+                        help='set level of logger')
+
+
+def run():
+    args = get_args()
+
+    now = datetime.now()
+    logger.warning('>>> job start at ' + now.strftime('%Y-%m-%d %H:%M:%S'))
+    state = main(*args)
+    logger.warning('>>> job run time: ' + str(datetime.now() - now))
+    if state == 0:
+        logger.info('success!')
+
+
+if __name__ == '__main__':
+    run()
