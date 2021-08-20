7be214dfd158796f7fd18c300449bf796257c0e9
Hwrn <1322053653@qq.com>
2021-08-20 17:44:00 +0800

    tree things

--- Bash/treee.sh
+++ Bash/treee.sh
@@ -1,4 +1,3 @@
-tree -DFqs --timefmt "%F %T" --du \
+tree -DFqs --timefmt "%F %T" --du $@ \
 | awk -v FS='[\]\[]' '{if ($2) {print "["$2"] "$1$3} else {print $0}}' 2>/dev/null \
 | awk -v FS='──   '  '{if ($2) {print $1"── "$2}     else {print $0}}'
-
