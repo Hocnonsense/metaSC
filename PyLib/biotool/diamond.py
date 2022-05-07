3f3dd176f92396cae99e348fc920850a41491a21
Hwrn <1322053653@qq.com>
2022-05-07 14:15:49 +0800

    diamondmulti threads

--- PyLib/biotool/diamond.py
+++ PyLib/biotool/diamond.py
@@ -2,7 +2,7 @@
 """
  * @Date: 2021-09-23 17:29:53
  * @LastEditors: Hwrn
- * @LastEditTime: 2022-05-06 16:36:02
+ * @LastEditTime: 2022-05-07 14:10:46
  * @FilePath: /metaSC/PyLib/biotool/diamond.py
  * @Description: Run diamond with python
 """
@@ -20,14 +20,14 @@ def remove_suffix(name: str, suffix=""):
     return name[: -len(suffix)] if name.endswith(suffix) else name
 
 
-def diamond_check_db(fastapath: Path, outdir: Path):
+def diamond_check_db(fastapath: Path, outdir: Path, threads=1):
     """Make database with DIAMOND."""
     diamond_db = outdir / remove_suffix(fastapath.name, ".faa")
     cmd = (
         f"diamond makedb "
         f"    --in {fastapath} "
         f"    -d {diamond_db} "
-        f"    -p 1 --quiet"
+        f"    -p {threads} --quiet"
     )
     logger.info(cmd)
     if not os.access(diamond_db, os.F_OK):
@@ -35,12 +35,14 @@ def diamond_check_db(fastapath: Path, outdir: Path):
     return diamond_db
 
 
-def diamond_blastp(query: Path, db: Path, level="sensitive", outdir: Path = Path(".")):
+def diamond_blastp(
+    query: Path, db: Path, level="sensitive", outdir: Path = Path("."), threads=1
+):
     """DIAMOND blastp alignment."""
     level = "" if level == "fast" else f"--{level}"
 
     # make database with DIAMOND
-    diamond_db = diamond_check_db(db, outdir)
+    diamond_db = diamond_check_db(db, outdir, threads)
 
     blast_out = outdir / (
         remove_suffix(query.name, ".faa")
@@ -54,7 +56,7 @@ def diamond_blastp(query: Path, db: Path, level="sensitive", outdir: Path = Path
         f"    -q {query} "
         f"    -o {blast_out} "
         f"    -k 10 -e 0.001 --id 30 --query-cover 70 --subject-cover 70 "
-        f"    -b 10 --dbsize 1000000000 -p 1 "
+        f"    -b 10 --dbsize 1000000000 -p {threads} "
         f"    --quiet {level}"
     )
     logger.info(cmd)
@@ -63,15 +65,21 @@ def diamond_blastp(query: Path, db: Path, level="sensitive", outdir: Path = Path
     return blast_out
 
 
-def diamond_1to1(genome1: Path, genome2: Path, out: Path, sensitive_level="sensitive"):
+def diamond_1to1(
+    genome1: Path, genome2: Path, out: Path, sensitive_level="sensitive", threads=1
+):
     with tempfile.TemporaryDirectory() as tempdir_:
         tempdir = Path(tempdir_)
         basicConfig()
         logger.debug(f"diamand: ({tempdir}) : {genome1}, {genome2}")
         for i in range(1):
             try:
-                out1 = diamond_blastp(genome1, genome2, sensitive_level, tempdir)
-                out2 = diamond_blastp(genome2, genome1, sensitive_level, tempdir)
+                out1 = diamond_blastp(
+                    genome1, genome2, sensitive_level, tempdir, threads
+                )
+                out2 = diamond_blastp(
+                    genome2, genome1, sensitive_level, tempdir, threads
+                )
                 logger.debug(
                     f"filter diamand finished: {tempdir} : {genome1}, {genome2}"
                 )
