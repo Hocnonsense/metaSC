739e2bfc86125213ba5ba4518c1b779b64fe149a
Hwrn <1322053653@qq.com>
2022-05-19 22:09:54 +0800

    probably ... a few more stable

--- R/RLib.r
+++ R/RLib.r
@@ -1,10 +1,10 @@
 ###
-#* @Date: 2022-02-27 13:23:46
-#* @LastEditors: Hwrn
-#* @LastEditTime: 2022-04-25 00:50:33
-#* @FilePath: /metaSC/R/RLib.r
-#* @Description:
-#     source me!
+#' @Date: 2022-02-27 13:23:46
+#' @LastEditors: Hwrn
+#' @LastEditTime: 2022-05-19 22:09:23
+#' @FilePath: /metaSC/R/RLib.r
+#' @Description:
+#'    source me!
 ###
 
 
@@ -32,19 +32,18 @@ library(DESeq2)
 library(edgeR)
 
 suppressMessages(library(pathview))
-#install.packages("roxygen2", depend = TRUE)
-library(roxygen2)
 
 reload.RLib <- function() {
-  dir <- "."
+  RLib.dir <- getSrcDirectory(function(x) {x})
   if (sys.nframe() > 0) {
     frame <- sys.frame(1)
     if (!is.null(frame$ofile)) {
-      dir <- dirname(frame$ofile)
+      RLib.dir <- dirname(frame$ofile)
     }
   }
-  abs.dir = file.path(dir, "RLib/")
-  roxygenize(abs.dir)
+  abs.dir = file.path(RLib.dir, "RLib/")
+  #install.packages("roxygen2", depend = TRUE)
+  roxygen2::roxygenize(abs.dir)
   system(paste("R CMD build", abs.dir))
   #system(paste("R CMD check", abs.dir))
   system(paste("R CMD INSTALL", abs.dir))
