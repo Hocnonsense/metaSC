a26ab69f8215a71ceb03cf9a45c66bfdc9c4239e
Hwrn <hwrn.aou@sjtu.edu.cn>
2023-02-02 20:55:35 +0800

    fix: fix and format

--- R/RLib/R/total_percent_bar.r
+++ R/RLib/R/total_percent_bar.r
@@ -1,10 +1,13 @@
 ###
 #' @Date: 2022-06-28 15:37:09
 #' @LastEditors: Hwrn hwrn.aou@sjtu.edu.cn
-#' @LastEditTime: 2023-01-17 17:27:41
+#' @LastEditTime: 2023-02-02 17:53:46
 #' @FilePath: /2022_09-M_mem/workflow/utils/libs/metaSC/R/RLib/R/total_percent_bar.r
 #' @Description:
 ###
+
+suppressMessages(library(dplyr))
+suppressMessages(library(ggplot2))
 
 
 #' @title get total abundance of div.otu
@@ -24,65 +27,80 @@
 #'                values  : (<any>  ), <any>        , <numeric>   , <*any>
 #'                example : (Sample ), Sample       , Total       , Group, Layer
 #'
-get_total_ce <- function(div.raw, value.name, sample.name = "Sample", keep.cols = c("Group", "Layer")) {
-  keep.div.otu =
-    div.raw %>%
-    {.$..Sample = .[, sample.name]; .} %>%
-    .[c("..Sample", sample.name, keep.cols)] %>%
-    unique
+get_total_ce <-
+  function(div.raw, value.name,
+           sample.name = "Sample",
+           keep.cols = c("Group", "Layer")) {
+    keep.div.otu <-
+      div.raw %>%
+      {
+        .$..Sample <- .[, sample.name]
+        .
+      } %>%
+      .[c("..Sample", sample.name, keep.cols)] %>%
+      unique()
 
-  div.raw %>%
-    {.$..Sample = .[, sample.name]; .} %>%
-    {.$..All = .[, value.name]; .} %>%
-    group_by(..Sample) %>%
-    dplyr::summarise(Total = sum(..All)) %>%
-    merge(keep.div.otu) %>%
-    data.frame(row.names = .$..Sample) %>%
-    {.$..Sample <- NULL; .}
-}
+    div.raw %>%
+      group_by(..Sample = .[, sample.name]) %>%
+      dplyr::summarise(Total = sum(get(value.name))) %>%
+      merge(keep.div.otu) %>%
+      data.frame(row.names = .$..Sample) %>%
+      {
+        .$..Sample <- NULL
+        .
+      }
+  }
 
 
 get_total_plot <- function(
-  div.raw, value.name, sample.name = "Sample", fill = "Group", labs.x = "", labs.y = "",
-  font_size_1 = 15, font_size_2 = 13, font_size_3 = 10, axis.ticks.length = 0.1
-) {
-  total_ce = get_total_ce(div.raw, value.name)
+    div.raw, value.name,
+    sample.name = "Sample", fill = "Group",
+    labs.x = "", labs.y = "",
+    font_size_1 = 15, font_size_2 = 13, font_size_3 = 10,
+    axis.ticks.length = 0.1) {
+  total_ce <- get_total_ce(div.raw, value.name)
 
-  p_total_mapped_rate =
-    ggplot(data = total_ce,
-           mapping = aes_string(x = sample.name, y = "Total", fill = fill)) +
-    geom_bar(stat = "identity",
-             col = "black", size = 0.3) +
-    #scale_y_continuous(labels = ~format(.x, scientific = TRUE) %>%
-    #                     str_replace("e\\+0", "%*%10^") %>%
-    #                     parse(text = .)) +
+  p_total_mapped_rate <-
+    ggplot(
+      data = total_ce,
+      mapping = aes_string(x = sample.name, y = "Total", fill = fill)
+    ) +
+    geom_bar(
+      stat = "identity",
+      col = "black", size = 0.3
+    ) +
     scale_x_discrete(limits = rownames(total_ce), labels = NULL) +
-    scale_fill_manual(values = sample_meta_col) +
-    #geom_hline(data = data.frame(y = min_rarefy_threshold),
-    #           mapping = aes_string(yintercept = "y", linetype = "paste(y)"),
-    #           color = "red") +
-    #guides(linetype = guide_legend(title = "threshold")) +
     labs(title = "", x = labs.x, y = labs.y) +
-    theme(panel.grid = element_blank(),
-          panel.background = element_blank(),
-          panel.border = element_blank(),
-          axis.text = element_text(size = font_size_2, colour = "black", face = "bold"),
-          axis.title = element_text(size = font_size_1, face = "bold", colour = "black"),
-          axis.ticks.length = unit(0, 'cm'),
-          legend.title = element_text(size = font_size_2, face = "bold"),
-          legend.key = element_blank())
+    theme(
+      panel.grid = element_blank(),
+      panel.background = element_blank(),
+      panel.border = element_blank(),
+      axis.text = element_text(
+        size = font_size_2, colour = "black", face = "bold"
+      ),
+      axis.title = element_text(
+        size = font_size_1, face = "bold", colour = "black"
+      ),
+      axis.ticks.length = unit(0, "cm"),
+      legend.title = element_text(size = font_size_2, face = "bold"),
+      legend.key = element_blank()
+    )
 
   return(p_total_mapped_rate)
 }
 
 
-get_relative_ce <- function(div.raw, value.name, sample.name = "Sample", total_ce = NULL) {
+get_relative_ce <- function(
+    div.raw, value.name, sample.name = "Sample",
+    total_ce = NULL) {
   if (is.null(total_ce)) {
-    total_ce = get_total_ce(div.raw, value.name, sample.name = sample.name)
+    total_ce <- get_total_ce(div.raw, value.name, sample.name = sample.name)
   }
   div.raw %>%
-    as.data.frame %>%
-    {.[, value.name] / total_ce[as.character(.[, sample.name]), "Total"] * 100}
+    as.data.frame() %>%
+    {
+      .[, value.name] / total_ce[as.character(.[, sample.name]), "Total"] * 100
+    }
 }
 
 #' @title draw a picture of relative abundance
@@ -98,92 +116,115 @@ get_relative_ce <- function(div.raw, value.name, sample.name = "Sample", total_c
 #' @param sample.name name of sample to define row names
 #' @return plot of relative abundance in each environment
 get_percent_plot <- function(
-  div.raw, value.name, fill.name, sample.name = "Sample",
-  labs.x = "", labs.y = "",
-  TOP_N_TAXON_PER_LAYER = NULL, MIN_REPORT_TAXON_PERCENT = 0,
-  font_size_1 = 15, font_size_2 = 13, font_size_3 = 10,
-  axis.ticks.length = 0.1,
-  geom_bar.color = "#454545"
-) {
-  total_ce = get_total_ce(div.raw, value.name, sample.name)
+    div.raw, value.name, fill.name, sample.name = "Sample",
+    labs.x = "", labs.y = "",
+    TOP_N_TAXON_PER_LAYER = NULL, MIN_REPORT_TAXON_PERCENT = 0,
+    font_size_1 = 15, font_size_2 = 13, font_size_3 = 10,
+    axis.ticks.length = 0.1,
+    geom_bar.color = "#454545") {
+  total_ce <- get_total_ce(div.raw, value.name, sample.name)
 
-  ce =
+  ce <-
     div.raw %>%
-    as.data.frame %>%
+    as.data.frame() %>%
     mutate(
       Abundance = get_relative_ce(., value.name, sample.name, total_ce)
     ) %>%
-    {.$name = .[, fill.name]; .} %>%
     {
-      if(!is.null(TOP_N_TAXON_PER_LAYER)) {
-        top.taxon =
+      .$name <- .[, fill.name]
+      .
+    } %>%
+    {
+      if (!is.null(TOP_N_TAXON_PER_LAYER)) {
+        top_taxon <-
           reshape2::acast(
             ., formula(paste0("name ~ ", sample.name)),
             value.var = "Abundance", fill = 0, fun.aggregate = sum
           ) %>%
-          otu.div.top(TOP_N_TAXON_PER_LAYER, min.value = 0) %>% sort
-      } else if(MIN_REPORT_TAXON_PERCENT > 0) {
-        top.taxon =
+          otu.div.top(TOP_N_TAXON_PER_LAYER, min.value = 0) %>%
+          sort()
+      } else if (MIN_REPORT_TAXON_PERCENT > 0) {
+        top_taxon <-
           split(.[c("Abundance", "name")], .[sample.name]) %>%
           lapply(
-            . %>% group_by(name) %>% summarise(Abundance = sum(Abundance))
+            . %>%
+              group_by(name = get("name")) %>%
+              summarise(Abundance = sum(get("Abundance")))
           ) %>%
           bind_rows(.id = sample.name) %>%
-          {.$name[.$Abundance >= 1]} %>%
-          {unique(.) %>% as.character}
+          {
+            .$name[.$Abundance >= MIN_REPORT_TAXON_PERCENT]
+          } %>%
+          {
+            unique(.) %>% as.character()
+          }
       } else {
-        top.taxon = NULL
+        top_taxon <- NULL
       }
-      if (!is.null(top.taxon)) {
-        .$name = .$name %>%
-          as.character %>%
-          {ifelse(. %in% top.taxon, ., "others")} %>%
-          factor(levels = c(top.taxon, "others"))
+      if (!is.null(top_taxon)) {
+        .$name <- .$name %>%
+          as.character() %>%
+          {
+            ifelse(. %in% top_taxon, ., "others")
+          } %>%
+          factor(levels = c(top_taxon, "others"))
       }
       .
     } %>%
-    {.}
+    {
+      .
+    }
 
-  p_relative_abundance =
-    ggplot(data = ce,
-           mapping = aes_string(x = sample.name, y = "Abundance",
-                                fill = "name")) +
-    geom_bar(position = position_stack(reverse = T),
-             stat = "identity",
-             color = geom_bar.color, size = 0.3) +
+  p_relative_abundance <-
+    ggplot(
+      data = ce,
+      mapping = aes_string(
+        x = sample.name, y = "Abundance",
+        fill = "name"
+      )
+    ) +
+    geom_bar(
+      position = position_stack(reverse = T),
+      stat = "identity",
+      color = geom_bar.color, size = 0.3
+    ) +
     paletteer::scale_fill_paletteer_d("ggsci::default_igv") +
     scale_x_discrete(limits = rownames(total_ce)) +
-    #guides(fill = "none") +
+    # guides(fill = "none") +
     guides(fill = guide_legend(title = fill.name, ncol = 1, reverse = TRUE)) +
-    #theme(axis.text.x = element_text(colour = axis.x.color)) +
+    # theme(axis.text.x = element_text(colour = axis.x.color)) +
     labs(title = "", x = labs.x, y = labs.y)
 
-  p1 =
+  p1 <-
     p_relative_abundance +
     theme(
-      panel.grid.major = element_blank(),  # element_line(color = 'gray', size = 0.2),  #
+      panel.grid.major = element_blank(),
       panel.grid.minor = element_blank(),
-      panel.background = element_blank(),  # element_rect(color = 'black', fill = 'transparent'),  #
-      panel.border = element_blank(),  # element_rect(fill = NA, colour = "black"),  #
+      panel.background = element_blank(),
+      panel.border = element_blank(),
     ) +
     theme(
       axis.line = element_line(colour = "black"),
-      axis.text = element_text(size = font_size_2, colour = "black", face = "bold"),
-      axis.title = element_text(size = font_size_1, face = "bold", colour = "black"),
-      axis.ticks.length = unit(axis.ticks.length, 'cm'),
-
+      axis.text = element_text(
+        size = font_size_2, colour = "black", face = "bold"
+      ),
+      axis.title = element_text(
+        size = font_size_1, face = "bold", colour = "black"
+      ),
+      axis.ticks.length = unit(axis.ticks.length, "cm"),
       axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)
     ) +
     theme(
       legend.title = element_text(size = font_size_2, face = "bold"),
       legend.text = element_text(size = font_size_3),
-      legend.key = element_blank()  # element_rect(fill = "gray")
-      #legend.position = "bottom"
+      legend.key = element_blank()
     ) +
-    theme(text = element_text(family = "Arial",
-                              size = font_size_1,
-                              hjust = 0.5,
-                              lineheight = 0.5)) +
+    theme(text = element_text(
+      family = "Arial",
+      size = font_size_1,
+      hjust = 0.5,
+      lineheight = 0.5
+    )) +
     theme(plot.title = element_text(hjust = 0.5))
 
   return(p1)
