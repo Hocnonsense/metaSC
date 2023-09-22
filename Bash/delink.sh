1bce01a195dae4173e6a890a9c72c8e85dd619e4
Hwrn <hwrn.aou@sjtu.edu.cn>
2023-09-22 22:46:53 +0800

    fix: fix action and add docs

--- Bash/delink.sh
+++ Bash/delink.sh
@@ -10,31 +10,94 @@
 :<<!EOF!
  * @Date: 2023-08-26 20:45:01
  * @LastEditors: Hwrn hwrn.aou@sjtu.edu.cn
- * @LastEditTime: 2023-09-22 18:10:06
+ * @LastEditTime: 2023-09-22 20:45:06
  * @FilePath: /metaSC/Bash/delink.sh
  * @Description:
     change the linked path to a real path, and keep file in by link again
+    including both file and hide files
 !EOF!
 set -e
 
-declare path=$1
-declare real_path=`realpath $1`
+function show_usage (){
+    echo ""
+    echo "Usage: $(basename $0) [-a] path"
+    echo "Required arguments:"
+    echo "    path | path to change from link to folder"
+    echo "Optional arguments:"
+    echo "    -a | if set, hide file will be linked as well"
+    echo ""
+    echo "FILE output:"
+    echo "   Before:"
+    echo "      a -> b"
+    echo "      b/"
+    echo "          c"
+    echo "   After:"
+    echo "      a/"
+    echo "          c -> /real path to/b/c"
+    echo ""
 
-if [[ $path==$real_path ]]
+    return 0
+}
+declare use_a=""
+declare path=""
+
+
+set -- `getopt ha "$@"`
+
+while [ -n "$1" ]
+do
+    case "$1" in
+    -h)
+        show_usage
+        exit ;;
+    -a)
+        use_a="-a"
+        shift ;;
+    --) ;;
+    *)
+        path=$1
+        break ;;
+    esac
+    shift
+done
+
+if [ ! -e $path ]
+then
+    show_usage
+    echo "No path given, abort"
+    exit 1
+fi
+
+
+declare real_path=`realpath $path`
+
+if [ ! -L "$path" ]
 then
     echo "path is not a link, abort" >&2
     exit 1
 fi
-if [[ -f $real_path ]]
+if [ -f "$real_path" ]
 then
-    echo "path is not a path, skip" >&2
+    echo "path is not a dir, skip" >&2
     exit 0
 fi
 
-rm $1
-mkdir $1
-ln -s $real_path/* $1
+rm "$path"
+mkdir "$path"
 
-for i in $i
-do touch -achmr `realpath $i` $i
+echo update $path with $real_path >&2
+
+for j in `ls "$real_path" $use_a`
+do
+    if [ -d $path/"$j" ]
+    then
+        echo skip "$j"
+    elif [ -e $path/"$j" ]
+    then
+        ln -s "$real_path"/"$j" "$1"/"$j"
+        echo find broken link "$real_path"/"$j"
+    else
+        ln -s "$real_path"/"$j" "$1"/"$j"
+        touch -achmr `realpath "$real_path"/"$j"` "$1"/"$j" | printf ""
+    fi
 done
